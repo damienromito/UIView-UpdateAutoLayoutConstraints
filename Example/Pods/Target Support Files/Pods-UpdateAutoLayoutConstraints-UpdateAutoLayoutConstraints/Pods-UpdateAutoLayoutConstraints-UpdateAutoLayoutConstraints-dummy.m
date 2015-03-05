8fe2fdcae1122c97b97ebe4b139fe26f6d03e7d9
Damien <damien.romito@gmail.com>
2015-03-05 14:27:20 +0100

    add cocoapod spec

--- Example/Pods/Target Support Files/Pods-UpdateAutoLayoutConstraints-UpdateAutoLayoutConstraints/Pods-UpdateAutoLayoutConstraints-UpdateAutoLayoutConstraints-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_UpdateAutoLayoutConstraints_UpdateAutoLayoutConstraints : NSObject
-@end
-@implementation PodsDummy_Pods_UpdateAutoLayoutConstraints_UpdateAutoLayoutConstraints
-@end
